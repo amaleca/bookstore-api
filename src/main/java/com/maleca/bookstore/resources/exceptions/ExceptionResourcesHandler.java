15d2b269e2c8bd07d73014f5922f5c4f58f187c5
alfredomaleca <alfredomaleca55@gmail.com>
2023-08-01 18:15:25 +0200

    Tratamento de Erros Personalizados

---
+++ src/main/java/com/maleca/bookstore/resources/exceptions/ExceptionResourcesHandler.java
@@ -0,0 +1,24 @@
+package com.maleca.bookstore.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.maleca.bookstore.service.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.ServletRequest;
+
+@ControllerAdvice
+public class ExceptionResourcesHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	
+	public ResponseEntity<StandardError> objectNotFoundException (ObjectNotFoundException e, ServletRequest request){
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	} 
+
+}
--- src/main/java/com/maleca/bookstore/service/CategoriaService.java
+++ src/main/java/com/maleca/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.maleca.bookstore.domain.Categoria;
 import com.maleca.bookstore.repositories.CategoriaRepository;
+import com.maleca.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto nao encontrado! id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
